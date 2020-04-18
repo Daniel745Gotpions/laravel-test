3cbb307155c172530196ee130184f083bfbb4a63
danielb <daniel.berkovich@finantick.com>
2020-04-18 14:00:09 +0300

    first push

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-lg-3 p-5 text-center">
+            <img style="width: 80%" src="/images/emoticon+happy+joy+laugh+reaction+smile+icon-1320190653175355610.png">
+        </div>
+        <div class="col-lg-9 pt-5">
+            <div class=""><h1>freeCodeCamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>100K</strong> Posts</div>
+                <div class="pr-5"><strong>23K</strong> Followers</div>
+                <div class="pr-5"><strong>229K</strong> Following</div>
+            </div>
+            <div class="pt-3 font-weight-bold">freecodecamp.org</div>
+            <div>
+                orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised i
+            </div>
+            <div><a href="#">www.freecodecamp.org</a></div>
+        </div>
+    </div>     
+</div>
+@endsection
